27d16671b67958f143d98d57f5f6a8de71f8484a
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2021-02-16 15:42:06 -0800

    updates readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
+const url = 'https://jsonplaceholder.typicode.com/users'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url).then((result) => result.json())
+      setData([...res])
+    }
+    getData()
+  }, [])
+
+  const firstNames = []
+
+  for (let name of data) {
+    firstNames.push(name.name)
+  }
+
+  let connectedNames = firstNames.join(', ')
+  console.log(firstNames)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data
+        ?.filter((person) => person.name[0] !== 'M')
+        .filter((person) => person.name[0] !== 'K')
+        .map((item) => {
+          return (
+            <section key={item.id} className="contact">
+              <div>
+                {item.name} | {item.email}
+              </div>
+              <div className="phone">
+                <p>#</p>{' '}
+                <p className="center">
+                  {item.phone.replace(/[)]/g, '-').replace(/[(]/g, '-')}
+                </p>
+                <p> </p>
+              </div>
+            </section>
+          )
+        })}
+      <p className="numberThree">{connectedNames}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
